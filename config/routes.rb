0f8dacd34149a5306d0a48f156140621aae5271f
lucas nunes de sousa <lucasnshunter@hotmail.com>
2016-07-27 13:51:57 +0000

    iniciando o projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'site/home'
+
   resources :comments
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
